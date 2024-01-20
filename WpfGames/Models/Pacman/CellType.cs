e0f43b1a2b76300ff98cce5d6a747f95715bbe75
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-01-20 22:19:41 +0300

    Added CellType enum for map loading

---
+++ WpfGames/Models/Pacman/CellType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfGames.Models.Pacman
+{
+    public enum CellType
+    {
+        Empty,
+        Wall,
+        Coin
+    }
+}
