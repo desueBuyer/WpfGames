a5b619daece1ded4a2510b0181df602467b1a5f8
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-01-20 23:40:18 +0300

    Addded MapLoader class

---
+++ WpfGames/Helpers/MapLoader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using WpfGames.Models.Pacman;
+
+namespace WpfGames.Helpers
+{
+    internal class MapLoader
+    {
+
+        private string _mapFilePath;
+
+        public string MapFilePath
+        {
+            get => _mapFilePath;
+            set => _mapFilePath = value;    
+        }
+
+        private string[] _rawMap;
+
+        public readonly List<List<CellType>> Map = new List<List<CellType>>();
+
+        public MapLoader(string mapFilePath)
+        {
+            _mapFilePath = mapFilePath;
+            LoadRawMap();
+            LoadFullMap();
+        }
+
+        private void  LoadRawMap()
+        {
+            _rawMap = File.ReadAllLines(_mapFilePath);
+        }
+
+        private void LoadFullMap()
+        {
+            for (int x = 0; x < _rawMap.GetLength(1); x++)
+            {
+                var mapRow = new List<CellType>();
+                for (int y = 0; y < _rawMap.GetLength(0); y++)
+                {
+                    var type = CellType.Empty;
+                    switch (_rawMap[y][x])
+                    {
+                        case '.':
+                            type = CellType.Coin;
+                            break;
+                        case '#':
+                            type = CellType.Wall;
+                            break;
+                        default:
+                            break;
+                    }
+                    mapRow.Add(type);
+                }
+                Map.Add(mapRow);
+            }
+        }
+    }
+}
