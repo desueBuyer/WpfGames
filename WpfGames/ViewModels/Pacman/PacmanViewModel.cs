927635cbf294d3c0c023da4786d95f7a8ce14914
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-01-21 07:15:15 +0300

    Added PacmanViewModel

---
+++ WpfGames/ViewModels/Pacman/PacmanViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfGames.Models.Pacman;
+
+namespace WpfGames.ViewModels.Pacman
+{
+    internal class PacmanViewModel:BindableBase
+    {
+        
+        private PacmanModel _pModel;
+
+        public Canvas GameMap
+        {
+            get => _pModel.GameMap;
+            set
+            {
+                _pModel.GameMap = value;
+                RaisePropertyChanged(nameof(GameMap));
+            }
+        }
+
+        public PacmanViewModel()
+        {
+            _pModel = new PacmanModel();
+        }
+    }
+}
