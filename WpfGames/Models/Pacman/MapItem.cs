af391a05f6eb0cd67d8c22560654daf34e4f2bad
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-01-21 07:14:28 +0300

    Added MapItem class (object on game map)

--- WpfGames/Models/Pacman/MapItem.cs
+++ WpfGames/Models/Pacman/MapItem.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace WpfGames.Models.Pacman
 {
-    internal class MapItem
+    internal class MapItem : Image
     {
+        public int X{get; set;}
+        public int Y { get; set; }
+
+        public MapItem(string imagePath, int x, int y)
+        {
+            X = x;
+            Y = y;
+            this.Source = new BitmapImage(new Uri(imagePath));
+            this.Width = 16;
+            this.Height = 16;
+
+        }
     }
 }
---
+++ WpfGames/Models/Pacman/PacmanModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfGames.Helpers;
+
+namespace WpfGames.Models.Pacman
+{
+    internal class PacmanModel
+    {
+
+        private MapLoader _mLoader = new MapLoader(@"C:\Users\5\source\repos\WpfGames\WpfGames\Source\Map.txt");
+
+        public Canvas GameMap = new Canvas();
+
+        public PacmanModel()
+        {
+            BuildMap();
+        }
+
+
+        private void BuildMap()
+        {
+            for (int x = 0; x < _mLoader.Map.Count; x++)
+            {
+                for (int y = 0; y < _mLoader.Map[x].Count; y++)
+                {
+                    string imagePath;
+                    switch (_mLoader.Map[x][y])
+                    {
+                        case CellType.Coin:
+                            imagePath = @"C:\Users\5\source\repos\WpfGames\WpfGames\Source\Images\Dot.png";
+                            break;
+                        case CellType.Wall:
+                            imagePath = @"C:\Users\5\source\repos\WpfGames\WpfGames\Source\Images\Block.png";
+                            break;
+                        default:
+                            imagePath = string.Empty;
+                            break;
+                    }
+                    if (!string.IsNullOrEmpty(imagePath))
+                        AddMapItem(imagePath, x, y);
+                }
+            }    
+        }
+
+
+        private void AddMapItem(string imagePath, int x, int y)
+        {
+            var mapItem = new MapItem(imagePath, x, y);
+            Canvas.SetLeft(mapItem, x * 16);
+            Canvas.SetTop(mapItem, y * 16);
+            GameMap.Children.Add(mapItem);
+        }
+    }
+}
