ac3f5f57cfdb61cbf94bfa828d4a4da4010ac8f9
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-02-07 00:44:55 +0300

    Added Interface w coordinates

---
+++ WpfGames/Models/IMapObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfGames.Models
+{
+    interface IMapObject
+    {
+        int X { get; set; }
+        int Y { get; set; }
+    }
+}
