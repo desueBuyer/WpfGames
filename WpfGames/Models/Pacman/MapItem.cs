5b436ed7b5b5125e3cb1b8e004542d43e8fc8f08
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-02-07 00:44:12 +0300

    Added simple Init Methods

--- WpfGames/Models/Pacman/MapItem.cs
+++ WpfGames/Models/Pacman/MapItem.cs
@@ -8,19 +8,28 @@ using System.Windows.Media.Imaging;
 
 namespace WpfGames.Models.Pacman
 {
-    internal class MapItem : Image
+    internal class MapItem : Image, IMapObject
     {
         public int X{get; set;}
         public int Y { get; set; }
 
         public MapItem(string imagePath, int x, int y)
         {
+            Init(imagePath, x, y);
+        }
+
+        public void Init(string imagePath, int x, int y)
+        {
             X = x;
             Y = y;
+            LoadImage(imagePath);
+        }
+
+        public void LoadImage(string imagePath)
+        {
             this.Source = new BitmapImage(new Uri(imagePath));
             this.Width = 16;
             this.Height = 16;
-
         }
     }
 }
