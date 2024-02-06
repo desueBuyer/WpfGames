451ec8edbf5d0746ce6c97d04d7decf988feffbf
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-02-07 00:44:39 +0300

    Added inheritance from MapItem

---
+++ WpfGames/Models/Pacman/PLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfGames.Models.Pacman
+{
+    class PLayer : MapItem
+    {
+
+        public PLayer(string imagePath, int x, int y):base(imagePath, x, y)
+        {
+            Init(imagePath, x, y);
+        }
+
+
+    }
+}
